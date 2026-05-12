70e9ba2055d9d6f77957c9def497153655abc180
callmejojoe <legendaryjoezy@gmail.com>
2026-05-12 23:02:28 +0100

    navbar and overflow fix made the overflow for the hero more nice looking and added nice buttons for the navbar....that are functionally invisible💀

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,25 +1,30 @@
 'use client';
 import Link from "next/link";
+// import { Info } from "lucide-react"
+
+
 
 export default function Navbar(page) {
   return (
     <nav className="navbar">
-      <Link href="/" className={page === 'home' ? 'active-badge' : ''}>
+      <Link href="/" className={page === 'home' ? 'active-badge' : 'button'}>
         HOME
       </Link>
-      <Link href="/about" className={page === 'about' ? 'active-badge' : ''}>
+      <Link href="/about" className={page === 'about' ? 'active-badge' : 'button'}>
         ABOUT
       </Link>
-      <Link href="/portfolio" className={page === 'portfolio' ? 'active-badge' : ''}>
+      <Link href="/portfolio" className={page === 'portfolio' ? 'active-badge' : 'button'}>
         PORTFOLIO
       </Link>
-      <Link href="/projects" className={page === 'projects' ? 'active-badge' : ''}>
+      <Link href="/projects" className={page === 'projects' ? 'active-badge' : 'button'}>
         PROJECTS
       </Link>
       <span className="divider">|</span>
-      <Link href="/contact" className={page === 'contact' ? 'active-badge' : ''}>
-        CONTACT <span>ℹ</span>
+      <Link href="/contact" className={page === 'contact' ? 'active-badge' : 'button'}>
+        CONTACT <span className="info">i</span>
       </Link>
     </nav>
   );
 }
+
+{/* <Info size={16} /> */}
